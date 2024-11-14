1eb863f5d075dcbc1abe8b70e9185278cdb7bc86
Te1tester <107201091+Te1tester@users.noreply.github.com>
2024-11-14 18:06:41 +0700

    Update log.h

--- module/src/main/cpp/log.h
+++ module/src/main/cpp/log.h
@@ -7,10 +7,18 @@
 
 #include <android/log.h>
 
-#define LOG_TAG "Perfare"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define USELOGFUNCTION
 
+#define LOG_TAG "Perfare"
+#ifdef USELOGFUNCTION
+  #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+  #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+  #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+  #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#else
+  #define LOGD(...) // do nothing
+  #define LOGW(...) // do nothing
+  #define LOGE(...) // do nothing
+  #define LOGI(...) // do nothing
+#endif
 #endif //ZYGISK_IL2CPPDUMPER_LOG_H
