bf607cee66c20f6884bd87b17e4bd8964ae6ff0d
Te1tester <107201091+Te1tester@users.noreply.github.com>
2024-11-14 11:19:46 +0700

    Update hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -16,6 +16,11 @@
 #include <sys/mman.h>
 #include <linux/unistd.h>
 #include <array>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include "log.h"
 
 void hack_start(const char *game_data_dir) {
     bool load = false;
@@ -36,6 +41,40 @@ void hack_start(const char *game_data_dir) {
     }
     if (!load) {
         LOGI("libil2cpp.so not found in thread %d", gettid());
+    }
+}
+
+bool copyFile(const std::string& source, const std::string& destination) {
+    std::ifstream src(source, std::ios::binary);
+    std::ofstream dest(destination, std::ios::binary);
+    
+    // Check if the source file is open
+    if (!src.is_open() || !dest.is_open()) {
+       LOGE("copyFile error: Failed to open source or destination file.");
+        return false;
+    }
+    
+    // Copy data from source to destination
+    dest << src.rdbuf();
+    
+    src.close();
+    dest.close();
+    
+    return true;
+}
+
+std::string readFile(const std::string& filePatch){
+    std::ifstream file(filePatch);
+    std::string content = "";
+    if (!file.is_open()) {
+        LOGE("Error opening file: %s",filePatch.c_str());
+        return "";
+    } else{
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        content = buffer.str();
+        file.close();
+        return content;
     }
 }
 
