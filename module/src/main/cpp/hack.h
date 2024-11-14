027683f01310a0ff080709d47ad08c492ea12b3b
Te1tester <107201091+Te1tester@users.noreply.github.com>
2024-11-14 11:02:20 +0700

    Update hack.h

--- module/src/main/cpp/hack.h
+++ module/src/main/cpp/hack.h
@@ -6,7 +6,12 @@
 #define ZYGISK_IL2CPPDUMPER_HACK_H
 
 #include <stddef.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
 void hack_prepare(const char *game_data_dir, void *data, size_t length);
+std::string readFile(const std::string& filePatch);
+bool copyFile(const std::string& source, const std::string& destination);
 
 #endif //ZYGISK_IL2CPPDUMPER_HACK_H
