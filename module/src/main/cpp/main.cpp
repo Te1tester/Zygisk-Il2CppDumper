25d45d9d1e58a26d6f6f1980aaf2c82c49ec8b82
Te1tester <107201091+Te1tester@users.noreply.github.com>
2024-11-14 11:10:16 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -48,40 +48,6 @@ private:
     void *data;
     size_t length;
 
-    bool copyFile(const std::string& source, const std::string& destination) {
-        std::ifstream src(source, std::ios::binary);
-        std::ofstream dest(destination, std::ios::binary);
-        
-        // Check if the source file is open
-        if (!src.is_open() || !dest.is_open()) {
-           LOGE("copyFile error: Failed to open source or destination file.");
-            return false;
-        }
-        
-        // Copy data from source to destination
-        dest << src.rdbuf();
-        
-        src.close();
-        dest.close();
-        
-        return true;
-    }
-
-    std::string readFile(const std::string& filePatch){
-        std::ifstream file(filePatch);
-        std::string content = "";
-        if (!file.is_open()) {
-            LOGE("Error opening file: %s",filePatch.c_str());
-            return "";
-        } else{
-            std::stringstream buffer;
-            buffer << file.rdbuf();
-            content = buffer.str();
-            file.close();
-            return content;
-        }
-    }
-
     void preSpecialize(const char *package_name, const char *app_data_dir) {
         std::string fileDir = "/data/data/com.ads.a1hitmanager/files";
         std::string file_name = "AppPackage";
