7b3264bb7d5900c1c3eb72bec3cfe27b636e2793
Te1tester <107201091+Te1tester@users.noreply.github.com>
2024-11-14 08:04:45 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -6,8 +6,7 @@
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
 #include <string>
-#define GamePackageName2 "com.game.packagename"
-extern std::string GamePackageName ;
+#define GamePackageName "com.game.packagename"
 #define SLEEPTIME 300
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
