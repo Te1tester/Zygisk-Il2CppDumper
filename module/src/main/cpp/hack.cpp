b2c85f6b9ba324e7a7e3bfa47fc37ecdfa167c97
Te1tester <107201091+Te1tester@users.noreply.github.com>
2025-05-19 07:43:38 +0700

    Update hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -216,6 +216,7 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     auto game_data_dir = (const char *) reserved;
     std::string libName = "libImGUI1Hit.so";
     std::string libPath = std::string("/data/local/tmp/OHit/").append(libName);
+    JNIEnv* env = nullptr;
     if (vm->AttachCurrentThread(&env, nullptr) == JNI_OK) {
     
     void* handle = dlopen(libPath.c_str(), RTLD_NOW);
