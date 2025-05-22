f51f2c213a49946a43fa3c8ad8a6e869e238cb58
Te1tester <107201091+Te1tester@users.noreply.github.com>
2025-05-22 09:54:59 +0700

    Update hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -123,8 +123,8 @@ struct NativeBridgeCallbacks {
 
 bool NativeBridgeLoad(const char *game_data_dir, int api_level, void *data, size_t length) {
     //TODO 等待houdini初始化
-    //sleep(5);
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    sleep(3);
+    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
     void* libart = 0;
     while (libart == 0){
         libart = dlopen("libart.so", RTLD_NOW);
