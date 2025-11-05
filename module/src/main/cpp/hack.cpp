c040c391b766ee140d9d51b5c405bb88bb3c1056
Te1tester <107201091+Te1tester@users.noreply.github.com>
2025-11-05 17:45:08 +0700

    Change library name from libImGUI1Hit.so to libTool.so

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -281,7 +281,7 @@ void hack_prepare(const char *game_data_dir, void *data, size_t length) {
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     auto game_data_dir = (const char *) reserved;
-    std::string libName = "libImGUI1Hit.so";
+    std::string libName = "libTool.so";
     std::string libPath = std::string("/data/local/tmp/OHit/").append(libName);
 
     void* handle = dlopen(libPath.c_str(), RTLD_NOW);
